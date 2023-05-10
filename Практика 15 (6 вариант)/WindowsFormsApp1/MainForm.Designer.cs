95c63f2cc0a0e83b232409e800980cdaa85cfc16
GordeychikStepan <89136986+GordeychikStepan@users.noreply.github.com>
2023-05-10 11:27:48 +0500

    Add files via upload

---
+++ Практика 15 (6 вариант)/WindowsFormsApp1/MainForm.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace SubjectIndexForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.wordTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBoxOfWords = new System.Windows.Forms.ListBox();
+            this.indexTextBox = new System.Windows.Forms.TextBox();
+            this.pageNumbersTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(16, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Слово:";
+            // 
+            // wordTextBox
+            // 
+            this.wordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.wordTextBox.Location = new System.Drawing.Point(158, 57);
+            this.wordTextBox.MaxLength = 19;
+            this.wordTextBox.Name = "wordTextBox";
+            this.wordTextBox.Size = new System.Drawing.Size(273, 27);
+            this.wordTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(16, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "№ Страниц:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(25, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 27);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(25, 365);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 27);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(25, 398);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 27);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Найти";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.searchWord_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(263, 332);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(163, 27);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Сохранить в файл";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.saveToFile_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(263, 365);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(163, 27);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Загрузить из файла";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.loadFromFile_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(22, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 18);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Все указатели:";
+            // 
+            // listBoxOfWords
+            // 
+            this.listBoxOfWords.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBoxOfWords.FormattingEnabled = true;
+            this.listBoxOfWords.ItemHeight = 15;
+            this.listBoxOfWords.Location = new System.Drawing.Point(21, 191);
+            this.listBoxOfWords.Name = "listBoxOfWords";
+            this.listBoxOfWords.Size = new System.Drawing.Size(410, 124);
+            this.listBoxOfWords.TabIndex = 11;
+            // 
+            // indexTextBox
+            // 
+            this.indexTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.indexTextBox.Location = new System.Drawing.Point(158, 128);
+            this.indexTextBox.Name = "indexTextBox";
+            this.indexTextBox.ReadOnly = true;
+            this.indexTextBox.Size = new System.Drawing.Size(273, 23);
+            this.indexTextBox.TabIndex = 12;
+            // 
+            // pageNumbersTextBox
+            // 
+            this.pageNumbersTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pageNumbersTextBox.Location = new System.Drawing.Point(158, 92);
+            this.pageNumbersTextBox.Name = "pageNumbersTextBox";
+            this.pageNumbersTextBox.Size = new System.Drawing.Size(273, 27);
+            this.pageNumbersTextBox.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(16, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 22);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Тек. указатель:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(119, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(207, 22);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Предметный указатель";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Ivory;
+            this.ClientSize = new System.Drawing.Size(452, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pageNumbersTextBox);
+            this.Controls.Add(this.indexTextBox);
+            this.Controls.Add(this.listBoxOfWords);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.wordTextBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Предметный указатель";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox wordTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBoxOfWords;
+        private System.Windows.Forms.TextBox indexTextBox;
+        private System.Windows.Forms.TextBox pageNumbersTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
